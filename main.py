1cab98e51c08becf41a7103e860cd3a6df9be250
Alberto Vazquez <albertovmrm@gmail.com>
2021-07-21 13:40:12 +0200

    New option for num page searching

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import os
 from tkinter import ttk
 from tkinter import messagebox
+from PIL import ImageTk,Image
 
 class MainFrame:
     """ Class that contains the main items of the frame """
@@ -32,7 +33,7 @@ class MainFrame:
         self.province_var = tk.StringVar()
         #self.province_var.set(self.province_value_list[0])
         self.province_var.trace("w", self.update_towns_menu)
-        self.dropdown_province = ttk.Combobox(master, state="readonly", textvariable=self.province_var, values=self.province_value_list)
+        self.dropdown_province = ttk.Combobox(master, width=20, state="readonly", textvariable=self.province_var, values=self.province_value_list)
         self.dropdown_province.configure(font=main_font)
         self.dropdown_province.place(x=120,y=100)
 
@@ -42,7 +43,7 @@ class MainFrame:
         self.town_value_list = []
 
         self.town_var = tk.StringVar()
-        self.dropdown_town = ttk.Combobox(master, state=tk.DISABLED, textvariable=self.town_var)
+        self.dropdown_town = ttk.Combobox(master, width=20, state=tk.DISABLED, textvariable=self.town_var)
         self.dropdown_town.configure(font=main_font)
         self.dropdown_town.place(x=120,y=140)
 
@@ -51,7 +52,7 @@ class MainFrame:
 
         self.housing_var = tk.StringVar()
         self.housing_var.set(crawler.housing_types[0])
-        self.dropdown_menu_flat_type = ttk.Combobox(master, state="readonly", textvariable = self.housing_var, values=crawler.housing_types)
+        self.dropdown_menu_flat_type = ttk.Combobox(master, width=15, state="readonly", textvariable = self.housing_var, values=crawler.housing_types)
         self.dropdown_menu_flat_type.configure(font=main_font)
         self.dropdown_menu_flat_type.place(x=80, y=188)
 
@@ -69,46 +70,72 @@ class MainFrame:
         self.lbl_optional2 = tk.Label(master, font=small_font, bg="white", fg="gray", text= "(Opcional)")
         self.lbl_optional2.place(x=370,y=282)
 
+        self.lbl_num_page_search = tk.Label(master, font=main_font, bg="white", text= "Número de páginas")
+        self.lbl_num_page_search.place(x=30,y=320)
+
+        self.num_page_search = tk.StringVar()
+        self.num_page_search.set(1)
+        self.dropdown_num_page_search = ttk.Combobox(master, width=7, state="readonly", textvariable=self.num_page_search, values=[1,2,3,4,5,"6 o más"])
+        self.dropdown_num_page_search.configure(font=main_font)
+        self.dropdown_num_page_search.place(x=190, y=320)
+
+        self.info_image = Image.open("img/info.png")
+        self.info_image = self.info_image.resize((20, 20), Image.ANTIALIAS)
+        self.info_icon = ImageTk.PhotoImage(self.info_image)
+        self.info_icon_label = tk.Label(master, image=self.info_icon, bg="white")
+        self.info_icon_label.place(x=290, y=320)
+        self.info_icon_label.bind("<Enter>", self.on_enter)
+        self.info_icon_label.bind("<Leave>", self.on_leave)
+
         self.v_cb1 = tk.IntVar()
         self.checkbox1 = tk.Checkbutton(master, text="Milanuncios", variable=self.v_cb1, bg="white", onvalue=1, offvalue=0)
-        self.checkbox1.place(x=30,y=340)
+        self.checkbox1.place(x=30,y=380)
 
         self.v_cb2 = tk.IntVar()
         self.checkbox2 = tk.Checkbutton(master, text="Fotocasa", variable=self.v_cb2, bg="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox2.place(x=30,y=380)
+        self.checkbox2.place(x=30,y=420)
         
         self.v_cb3 = tk.IntVar()
         self.checkbox3 = tk.Checkbutton(master, text="Idealista", variable=self.v_cb3, bg="white", highlightcolor="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox3.place(x=30,y=420)
+        self.checkbox3.place(x=30,y=460)
 
         self.v_cb4 = tk.IntVar()
         self.checkbox4= tk.Checkbutton(master, text="Pisos.com", variable=self.v_cb4, bg="white", highlightcolor="white", onvalue=1, offvalue=0)
-        self.checkbox4.place(x=200,y=340)
+        self.checkbox4.place(x=200,y=380)
 
         self.v_cb5 = tk.IntVar()
         self.checkbox5= tk.Checkbutton(master, text="Vivados", variable=self.v_cb5, bg="white", highlightcolor="white", onvalue=1, offvalue=0)
-        self.checkbox5.place(x=200,y=380)
+        self.checkbox5.place(x=200,y=420)
 
         self.v_cb6 = tk.IntVar()
         self.checkbox6= tk.Checkbutton(master, text="Enalquier", variable=self.v_cb6, bg="white", highlightcolor="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox6.place(x=200,y=420)
+        self.checkbox6.place(x=200,y=460)
 
         self.v_cb7 = tk.IntVar()
         self.checkbox7= tk.Checkbutton(master, text="Yaencontre", variable=self.v_cb7, bg="white", highlightcolor="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox7.place(x=370,y=340)
+        self.checkbox7.place(x=370,y=380)
 
         self.v_cb8 = tk.IntVar()
         self.checkbox8= tk.Checkbutton(master, text="Tucasa", variable=self.v_cb8, bg="white", highlightcolor="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox8.place(x=370,y=380)
+        self.checkbox8.place(x=370,y=420)
 
         self.v_cb9 = tk.IntVar()
         self.checkbox9= tk.Checkbutton(master, text="Departiculares", variable=self.v_cb9, bg="white", highlightcolor="white", onvalue=1, offvalue=0, state=tk.DISABLED)
-        self.checkbox9.place(x=370,y=420)
+        self.checkbox9.place(x=370,y=460)
 
         self.checkboxes_list = [self.v_cb1, self.v_cb2, self.v_cb3, self.v_cb4, self.v_cb5, self.v_cb6, self.v_cb7, self.v_cb8, self.v_cb9]
 
         self.button_search = tk.Button(master, font=title_font, relief='groove', text="BUSCAR", command = lambda: self.search_matches(self.town_var.get(), self.province_var.get(), self.housing_var.get(), self.entry_max_price.get(), self.entry_min_price.get(), master))
         self.button_search.place(relx=0.5, rely=0.9, anchor=tk.CENTER)
+
+        self.info_label = tk.Label(master, font=small_font, fg="gray", bg="white", text="",anchor='w', justify=tk.LEFT)
+        self.info_label.place(x=200, y=350)
+
+    def on_enter(self, event):
+        self.info_label.configure(bg="gray90", relief=tk.GROOVE, text=" Este campo indica el número de \n páginas que se analizarán por cada \n uno de los sitios web seleccionados ")
+
+    def on_leave(self, enter):
+        self.info_label.configure(bg="white",relief=tk.FLAT, text="")
 
     def update_towns_menu(self, *args):
         """ Update town menu based on selected province """
@@ -194,7 +221,7 @@ def main():
     else:
         root.iconbitmap("@img/icon.xbm")
     
-    root.geometry("500x560")
+    root.geometry("500x600")
     root.resizable(False, False)
     root.configure(bg="white")
 
